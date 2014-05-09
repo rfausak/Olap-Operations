b40e0becbbe385cca2f4a07ebd4e5531633438df
x0tn1x <tieu.nguyen15@gmail.com>
2014-05-09 02:56:23 -0700

    Added a bit of design;put everything together to reflect Andrew's made changes

--- connection.php
+++ connection.php
@@ -6,7 +6,7 @@ $hostname = "localhost";
 //connection to the database
 $dbhandle = mysql_connect($hostname, $username, $password)
 or die("Unable to connect to MySQL");
-echo "Connected to MySQL<br>";
+//echo "Connected to MySQL<br>";
 
 //select a database to work with
 $selected = mysql_select_db("grocery",$dbhandle)
--- index.php
+++ index.php
@@ -1,36 +1,136 @@
+<?php
+include 'connection.php';
+/* 
+   Dynamic dropdown menu content generation (dimensions/attributes)
+   Copyright (c) 2014 Andrew R Fausak.  All Rights Reserved.
+*/
+$result1 = mysql_query("show columns from time") or die (mysql_error());
+$timeOptions = "";
+if (mysql_num_rows($result1) > 0) {
+$i = 0;
+    while ($row = mysql_fetch_assoc($result1)) {
+	$rowField = $row['Field'];
+	if($i !== 0){
+	$timeOptions .= '<option value="'.$rowField.'">'.$rowField.'</option>';
+	}
+	$i++;
+    }
+}
+mysql_free_result($result1);
+
+$result2 = mysql_query("show columns from promotion") or die (mysql_error());
+$promoOptions = "";
+if (mysql_num_rows($result2) > 0) {
+$i = 0;
+    while ($row = mysql_fetch_assoc($result2)) {
+        $rowField = $row['Field'];
+        if($i !== 0){
+        $promoOptions .= '<option value="'.$rowField.'">'.$rowField.'</option>';
+        }
+        $i++;
+    }
+}
+
+$result2 = mysql_query("show columns from store") or die (mysql_error());
+$storeOptions = "";
+if (mysql_num_rows($result2) > 0) {
+$i = 0;
+    while ($row = mysql_fetch_assoc($result2)) {
+        $rowField = $row['Field'];
+        if($i !== 0){
+        $storeOptions .= '<option value="'.$rowField.'">'.$rowField.'</option>';
+        }
+        $i++;
+    }
+}
+
+$result2 = mysql_query("show columns from product") or die (mysql_error());
+$productOptions = "";
+if (mysql_num_rows($result2) > 0) {
+$i = 0;
+    while ($row = mysql_fetch_assoc($result2)) {
+        $rowField = $row['Field'];
+        if($i !== 0){
+        $productOptions .= '<option value="'.$rowField.'">'.$rowField.'</option>';
+        }
+        $i++;
+    }
+}
+
+$result2 = mysql_query("show columns from sales_fact") or die (mysql_error());
+$salesOptions = "";
+if (mysql_num_rows($result2) > 0) {
+$i = 0;
+    while ($row = mysql_fetch_assoc($result2)) {
+        $rowField = $row['Field'];
+        if($i >= 4){
+        $salesOptions .= '<option value="'.$rowField.'">'.$rowField.'</option>';
+	}
+        $i++;
+    }
+}
+
+
+echo <<<HTML
+<center><h1>Welcome to Grocery's OLAP</h1></center>
 <center><form action="result.php" target="myframe" method="POST">
-<select name="timebox" id="timebox">
+<dl>
+<dt><label for="productbox">Product</label></dt>
+<dd><select name="productbox" id="productbox">
 <option value=""></option>
-<option value="year">Year</option>
-<option value="month">Month</option>
-</select>
+$productOptions
+</select></dd>
+</dl>
 
-<select name="productbox" id="productbox">
+<dl>
+<dt><label for="timebox">Time</label></dt>
+<dd><select name="timebox" id="timebox">
 <option value=""></option>
-<option value="description">Product Name</option>
-<option value="brand">Product Brand</option>
-</select>
+$timeOptions
+</select></dd>
+</dl>
 
-<select name="storebox" id="storebox">
+<dl>
+<dt><label for="storebox">Store</label></dt>
+<dd><select name="storebox" id="storebox">
 <option value=""></option>
-<option value="name">Store Name</option>
-<option value="store_number">Store Number</option>
-</select>
+$storeOptions
+</select></dd>
+</dl>
 
-<select name="promotionbox" id="promotionbox">
+<dl>
+<dt><label for="promotionbox">Promotion</label></dt>
+<dd><select name="promotionbox" id="promotionbox">
 <option value=""></option>
-<option value="promotion_name">Promotion Name</option>
-<option value="coupon_type">Coupton Type</option>
-</select>
+$promoOptions
+</select></dd>
+</dl>
 
-<select name="salesfactbox" id="salesfactbox">
+<dl>
+<dt><label for="salesfactbox">Sales Fact</label></dt>
+<dd><select name="salesfactbox" id="salesfactbox">
 <option value=""></option>
-<option value="dollar_sales">Dollar Sales</option>
-<option value="unit_sales">Unit Sales</option>
-</select>
-<input name="submitbutton" type="submit" value="submit" />
-</form></center>
+$salesOptions
+</select></dd>
+</dl>
+
+<input name="submitbutton" type="submit" value="submit" id="b1"/>
+</form>
+</center>
+HTML;
+?>
 
 <iframe name="myframe" src="result.php" width="100%" height="100%">
 	
 </iframe>
+
+
+<style>
+dl {
+	display: inline-block;
+}
+label {
+	font-weight:bold;
+	font-size: 20px;
+}
+</style>
